a455c1e9e44a849d1c28451f77f529ab51024113
Alexey <awa77@mail.ru>
2023-07-05 15:59:40 +0300

    Подправлен маленький баг + добавлен файт README.md

--- cdekpay/lib/config/guide.php
+++ cdekpay/lib/config/guide.php
@@ -2,7 +2,7 @@
 
 return array(
     array(
-        'value'       => '%RELAY_URL%/%APP_ID%/%MERCHANT_ID%',
+        'value'       => '%RELAY_URL%%APP_ID%/%MERCHANT_ID%',
         'title'       => 'URL для нотификации по HTTP',
         'description' => 'Значение настройки URL для оповещения активного протокола<br>
 <strong>Указанный в этом поле адрес скопируйте и сохраните в анкете подключения к приему платежей CDEK Pay</strong>',
